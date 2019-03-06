fac38cd219f7bc0a71e8911d14587c389ed88a5d
Julio Quevedo <juliovquevedo@gmail.com>
2019-03-06 12:28:48 -0500

    Added xRange and yRange which are the size of the data and its largest value

--- mychart.js
+++ mychart.js
@@ -15,7 +15,18 @@ function setChartDimensions () {
 function drawChart (chartWidth, chartHeight) {
     var c = document.getElementById("chart");
     var ctx = c.getContext("2d");
+    charData2 = [{x:0, y:1},
+                 {x:1, y:2},
+                 {x:2, y:3},
+                 {x:3, y:2},
+                 {x:4, y:1},
+                 {x:5, y:1},
+                ];
     
+    
+    
+    xRange = charData2.length;
+    yRange = getLargestValue(charData2, xRange);
     drawXAxis();
     drawYAxis();
     
@@ -32,5 +43,15 @@ function drawChart (chartWidth, chartHeight) {
         ctx.moveTo(chartWidth*.07,chartHeight*.95);
         ctx.lineTo(chartWidth*.07,chartHeight*.13);
         ctx.stroke();
+    }
+    
+    function getLargestValue (charData2, xRange) {
+        largest = 0;
+        for (m = 0; m < xRange; m++) {
+            if (charData2[m].y > largest) {
+                largest = charData2[m].y;
+            }
+        }
+        return largest;
     }
 }
